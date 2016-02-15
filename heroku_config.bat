f7299b683f3de459bff8f7fa3b5b29bb6d4edd0f
gorserg <gorserg@hotmail.com>
2016-02-15 16:57:15 +0200

    refactoring

---
+++ heroku_config.bat
@@ -0,0 +1,7 @@
+start heroku config:set GMAIL_USER=prozorro.crypto@gmail.com
+start heroku config:set GMAIL_PASSWORD=xxfxGjnwHGrO0E9GbF9N
+start heroku config:set CONTACT_EMAILS=prozorro.crypto@gmail.com,sergey.gorobets@unity-bars.com
+start heroku config:set SESSION_SECRET=secret
+start heroku config:set MONGOLAB_URI=mongodb://crypto_server:3nuSLftLWG55@ds055555.mongolab.com:55555/crypto_srv
+start heroku config:set GOOGLE_ID=669431064652-0ceaibsfba3hsj6vj4ibon00o21rb2ul.apps.googleusercontent.com
+start heroku config:set GOOGLE_SECRET=LNMD8niZkTBbZpK6TaOjfjGS
